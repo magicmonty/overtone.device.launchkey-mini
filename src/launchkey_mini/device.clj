3ec4107f9b741b5198dcac159249c2d843154aea
Martin Gondermann <magicmonty@pagansoft.de>
2015-03-08 13:07:52 +0100

    first device specific tests

--- src/launchkey_mini/device.clj
+++ src/launchkey_mini/device.clj
@@ -5,4 +5,42 @@
 
    [launchkey-mini.led :as led]))
 
+(def launchkey-mini-config {
+  :name "LK Mini InControl"
+  :interfaces {
+    :grid-controls {
+      :meta-keys {
+        :up      {:note 104 :type :control-change}
+        :down    {:note 105 :type :control-change}
+        :left    {:note 106 :type :control-change}
+        :right   {:note 107 :type :control-change}}
 
+      :side-controls {
+        :row1  {:note 104 :type :note-on :row 0}
+        :row2  {:note 120 :type :note-on :row 1}}
+
+      :knobs {
+        :knob1   {:note 21  :type :control-change}
+        :knob2   {:note 22  :type :control-change}
+        :knob3   {:note 23  :type :control-change}
+        :knob4   {:note 24  :type :control-change}
+        :knob5   {:note 25  :type :control-change}
+        :knob6   {:note 26  :type :control-change}
+        :knob7   {:note 27  :type :control-change}
+        :knob8   {:note 28  :type :control-change}}}
+
+    :leds {
+      :name "LEDs"
+      :type :midi-out
+      :midi-handle "LK Mini InControl"
+      :controls {
+        :row1  {:note 104 :fn midi-note-on}
+        :row2  {:note 120 :fn midi-note-on}}
+      :grid {
+        :fn midi-note-on}}}})
+
+(def side-controls (-> launchkey-mini-config :interfaces :grid-controls :side-controls keys))
+(def knobs (-> launchkey-mini-config :interfaces :grid-controls :knobs keys))
+(def meta-keys (-> launchkey-mini-config :interfaces :grid-controls :meta-keys keys))
+
+(defn side->row [name] (-> launchkey-mini-config :interfaces :grid-controls :side-controls name :row))
--- test/launchkey_mini/t_device.clj
+++ test/launchkey_mini/t_device.clj
@@ -2,5 +2,18 @@
   (:require [midje.sweet :refer :all]
             [launchkey-mini.device :as device]))
 
+(fact "correct side controls"
+  device/side-controls => (just [:row1 :row2] :in-any-order))
 
+(fact "correct knobs"
+  device/knobs => (just [:knob1 :knob2 :knob3 :knob4 :knob5 :knob6 :knob7 :knob8] :in-any-order))
 
+(fact "correct meta keys"
+  device/meta-keys => (just [:up :down :left :right] :in-any-order))
+
+(fact "side->row returns correct row"
+  (device/side->row :row1) => 0
+  (device/side->row :row2) => 1)
+
+(fact "side->row returns nil on wrong row"
+  (device/side->row :knob1) => nil)
