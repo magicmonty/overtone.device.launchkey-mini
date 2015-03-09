2265435311557726ac26eeb32c49807854c9cd6a
Martin Gondermann <magicmonty@pagansoft.de>
2015-03-09 22:02:32 +0100

    Grid handling

--- src/launchkey_mini/grid.clj
+++ src/launchkey_mini/grid.clj
@@ -1,11 +1,79 @@
 (ns launchkey-mini.grid)
 
+(defn fn-grid [] {})
+
 (def grid-width  8)
 (def grid-height 2)
 
 (def grid-notes
-  [(range 0x60 0x68)
-  (range 0x70 0x78)])
+  [(range 0x60 (+ 0x60 grid-width))
+  (range 0x70 (+ 0x70 grid-width))])
 
 (defn coordinate->note [row col]
   (-> grid-notes (nth (mod row grid-height)) (nth (mod col grid-width))))
+
+(defn- empty-row
+  ([] (empty-row grid-width))
+  ([row-width] (vec (map (fn [_] 0) (range 0 row-width)))))
+
+(defn virtual-grid
+  ([y-pages] (virtual-grid y-pages 1))
+  ([y-pages x-pages]
+    (vec (map (fn [_] (empty-row (* grid-width x-pages))) (range 0 (* grid-height y-pages))))))
+
+(defn empty-grid []
+  (virtual-grid 1))
+
+(defn x-offset [x x-page] (+ x (* x-page grid-width)))
+(defn y-offset [y y-page] (+ y (* y-page grid-height)))
+
+(defn project
+  ([full-grid] (project [0 0] full-grid))
+  ([[x-pos y-pos] full-grid]
+     (map (fn [row]
+            (let [new-row (->> row
+                               (drop (x-offset 0 x-pos))
+                               (take grid-width)
+                               seq)]
+              (or new-row (take grid-width (repeat 0)))))
+          (take grid-height (drop (y-offset 0 y-pos) full-grid)))))
+
+(defn x-page-count [grid] (int (/ (count (first grid)) grid-width)))
+(defn y-page-count [grid] (int (/ (count grid) grid-height)))
+(defn x-max [grid] (count (first grid)))
+(defn y-max [grid] (count grid))
+
+(defn toggle
+  ([grid row column] (toggle [0 0] grid row column))
+  ([[y-page x-page] grid row column]
+     (let [x-offset (x-offset column x-page)
+           y-offset (y-offset row y-page)
+           old-row (-> grid (nth y-offset) (vec))
+           old-cell (nth old-row x-offset)
+           new-row (assoc old-row x-offset (if (= 1 old-cell) 0 1))
+           new-grid (assoc (vec grid) y-offset new-row)]
+       new-grid)))
+
+(defn set-cell
+  "Set a cell within a grid to a value"
+  ([grid row column value] (set-cell [0 0] grid row column value))
+  ([[y-page x-page] grid row column value]
+     (let [y-offset (y-offset row y-page)
+           old-row (-> grid (nth y-offset) (vec))
+           new-row (assoc old-row (x-offset column x-page) value)]
+        (assoc (vec grid) y-offset new-row))))
+
+(defn cell
+  "Find the value of a cell"
+  ([grid row column] (cell [0 0] grid row column))
+  ([[y-page x-page] grid row column]
+     (let [x-offset (x-offset column x-page)
+           y-offset (y-offset row y-page)]
+       (-> grid
+           (nth y-offset)
+           (nth x-offset)))))
+
+(defn absolute-cell [grid row column]
+  (if (and (< row (y-max grid)) (< column (x-max grid)))
+    (-> grid (nth row) (nth column))
+    0))
--- test/launchkey_mini/t_grid.clj
+++ test/launchkey_mini/t_grid.clj
@@ -14,3 +14,83 @@
 
 (fact "coordinate->note wraps at y-overflow"
   (#'grid/coordinate->note 2 0) => 0x60)
+
+(fact "x-page-count returns correct count"
+  (#'grid/x-page-count (#'grid/virtual-grid 1 2)) => 2
+  (#'grid/x-page-count (#'grid/virtual-grid 2)) => 1
+  (#'grid/x-page-count (#'grid/empty-grid)) => 1)
+
+(fact "y-page-count returns correct count"
+  (#'grid/y-page-count (#'grid/virtual-grid 1 2)) => 1
+  (#'grid/y-page-count (#'grid/virtual-grid 2)) => 2
+  (#'grid/y-page-count (#'grid/empty-grid)) => 1)
+
+(fact "x-offset returns correct offset"
+  (#'grid/x-offset 0 0) => 0
+  (#'grid/x-offset 0 1) => 8
+  (#'grid/x-offset 0 2) => 16
+  (#'grid/x-offset 1 0) => 1
+  (#'grid/x-offset 1 1) => 9
+  (#'grid/x-offset 1 2) => 17)
+
+(fact "y-offset returns correct offset"
+  (#'grid/y-offset 0 0) => 0
+  (#'grid/y-offset 0 1) => 2
+  (#'grid/y-offset 0 2) => 4
+  (#'grid/y-offset 1 0) => 1
+  (#'grid/y-offset 1 1) => 3
+  (#'grid/y-offset 1 2) => 5)
+
+(fact "x-max returns grid total x size"
+  (#'grid/x-max (#'grid/empty-grid)) => 8
+  (#'grid/x-max (#'grid/virtual-grid 1 2)) => 16)
+
+(fact "y-max returns grid total y size"
+  (#'grid/y-max (#'grid/empty-grid)) => 2
+  (#'grid/y-max (#'grid/virtual-grid 2 1)) => 4)
+
+(fact "toggle toggles correct cell with default grid"
+  (#'grid/toggle (#'grid/empty-grid) 0 0) => [[1 0 0 0 0 0 0 0]
+                                              [0 0 0 0 0 0 0 0]]
+  (#'grid/toggle (#'grid/empty-grid) 1 5) => [[0 0 0 0 0 0 0 0]
+                                              [0 0 0 0 0 1 0 0]])
+
+(fact "toggle toggles correct cell with extended grid"
+  (#'grid/toggle [1 0] (#'grid/virtual-grid 2 1) 0 0) => [[0 0 0 0 0 0 0 0]
+                                                          [0 0 0 0 0 0 0 0]
+                                                          [1 0 0 0 0 0 0 0]
+                                                          [0 0 0 0 0 0 0 0]]
+  (#'grid/toggle [0 1] (#'grid/virtual-grid 1 2) 0 0) => [[0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0]
+                                                          [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]])
+
+(fact "set-cell sets given value to correct cell with default grid"
+  (#'grid/set-cell (#'grid/empty-grid) 0 0 5) => [[5 0 0 0 0 0 0 0]
+                                                  [0 0 0 0 0 0 0 0]]
+  (#'grid/set-cell (#'grid/empty-grid) 1 5 4) => [[0 0 0 0 0 0 0 0]
+                                                  [0 0 0 0 0 4 0 0]])
+
+(fact "set-cell sets given value to correct cell with extended grid"
+  (#'grid/set-cell [1 0] (#'grid/virtual-grid 2 1) 0 0 5) => [[0 0 0 0 0 0 0 0]
+                                                              [0 0 0 0 0 0 0 0]
+                                                              [5 0 0 0 0 0 0 0]
+                                                              [0 0 0 0 0 0 0 0]]
+  (#'grid/set-cell [0 1] (#'grid/virtual-grid 1 2) 0 0 3) => [[0 0 0 0 0 0 0 0 3 0 0 0 0 0 0 0]
+                                                              [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]])
+
+(fact "cell returns the correct value for a default grid"
+  (#'grid/cell [[1 2 3 4 5 6 7 8][0 0 0 0 0 0 0 0]] 0 3) => 4
+  (#'grid/cell [[0 0 0 0 0 0 0 0][1 2 3 4 5 6 7 8]] 1 5) => 6)
+
+(fact "cell returns the correct value for an extended grid"
+  (#'grid/cell [1 1]
+    [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0xB 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]] 0 3) => 0xB)
+
+(fact "absolute-cell returns the correct value for an extended grid"
+  (#'grid/absolute-cell
+    [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0xB 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]] 2 11) => 0xB)
