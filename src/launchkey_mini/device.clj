4635cc5f0c3f4131b3ec531f750e8d6e84968407
Martin Gondermann <magicmonty@pagansoft.de>
2015-03-11 21:15:30 +0100

    Added tests and comment for LED-Handling

--- src/launchkey_mini/device.clj
+++ src/launchkey_mini/device.clj
@@ -56,31 +56,34 @@
 (def disable-incontrol-msg (midi-msg :disable-incontrol))
 (def reset-launchkey-msg (midi-msg :reset-launchkey))
 
-(defn reset-launchkey* [sink]
+(defn- reset-launchkey* [sink]
   (midi/send-message sink reset-launchkey-msg))
 
 (defn reset-launchkey [launchkey-mini]
+  "Resets all LED states and the internal clock"
   (reset-launchkey* (-> launchkey-mini :rcv)))
 
-(defn enable-incontrol* [sink]
+(defn- enable-incontrol* [sink]
   (midi/send-message sink enable-incontrol-msg))
 
 (defn enable-incontrol [launchkey-mini]
+  "Enables InControl mode to be able to control the LEDs"
   (enable-incontrol* (-> launchkey-mini :rcv)))
 
-(defn disable-incontrol* [sink]
+(defn- disable-incontrol* [sink]
   (midi/send-message sink disable-incontrol-msg))
 
 (defn disable-incontrol [launchkey-mini]
+  "Disables InControl mode"
   (disable-incontrol* (-> launchkey-mini :rcv)))
 
-(defn led-details [id]
+(defn- led-details [id]
   (if (vector? id)
     {:note (apply grid/coordinate->note id) :fn (midi/midi-fn (-> launchkey-mini-config :interfaces :leds :grid :type))}
     (when-let [config (-> launchkey-mini-config :interfaces :leds :controls id)]
       {:note (config :note) :fn (midi/midi-fn (config :type))})))
 
-(defn led-on*
+(defn- led-on*
   ([sink id] (led-on* sink id 3 :amber))
   ([sink id brightness color]
     (when-let [{led-id :note midi-fn :fn} (led-details id)]
@@ -88,39 +91,48 @@
                                           :intensity brightness})))))
 
 (defn led-on
+  "turn on a LED via its coordinates ([row col]) for the pads
+  or its id (:row1, :row2) for the side buttons.
+  By default the color is :amber at full brightness
+  You can also provide a brightness (0-3) and a color"
   ([launchkeymini id] (led-on launchkeymini id led/full-brightness :amber))
   ([launchkeymini id brightness color]
       (let [sink (-> launchkeymini :rcv)]
         (led-on* sink id brightness color))))
 
-(defn led-off* [sink id]
+(defn- led-off* [sink id]
   (when-let [{led-id :note midi-fn :fn} (led-details id)]
     (midi-fn sink led-id 12)))
 
 (defn led-off [launchkeymini id]
+  "turn of a LED via its coordinates ([row col]) for the pads
+  or its id (:row1, :row2) for the side buttons."
   (let [sink (-> launchkeymini :rcv)]
     (led-off* sink id)))
 
-(defn led-flash-on* [sink id brightness color]
+(defn- led-flash-on* [sink id brightness color]
   (when-let [{led-id :note midi-fn :fn} (led-details id)]
     (midi-fn sink led-id (led/velocity {:color color :intensity brightness :mode :clear}))
     (Thread/sleep 200)
     (midi-fn sink led-id (led/velocity {:color color :intensity led/off :mode :clear}))))
 
 (defn led-flash-on
+  "briefly flash a LED via its coordinates ([row col]) for the pads
+  or its id (:row1, :row2) for the side buttons."
   ([launchkeymini id] (led-flash-on launchkeymini id led/full-brightness :amber))
   ([launchkeymini id brightness color]
     (let [sink (-> launchkeymini :rcv)]
       (led-flash-on* sink id brightness color))))
 
-(defn led-on-all* [sink]
+(defn- led-on-all* [sink]
   (midi/send-message sink {:type :control-change :note 0x00 :velocity 0x7D}))
 
 (defn led-on-all [launchkeymini]
+  "Enable LED test mode, wich switches all LEDs on (:amber at medium brightness)"
   (let [sink (-> launchkeymini :rcv)]
     (led-on-all* sink)))
 
-(defn render-row*
+(defn- render-row*
   ([sink row-data row] (render-row* sink row-data row led/full-brightness :amber))
   ([sink row-data row brightness color]
     (doseq [column (range 0 grid/grid-width)]
@@ -130,14 +142,17 @@
           (led-on* sink [row column] brightness color))))))
 
 (defn render-row
+  "Renders a complete row. You have to provide the row-data
+  as seqence of length launchkey-mini.grid/grid-length
+  and a row number (zero based)"
   ([launchkeymini row-data row] (render-row launchkeymini row-data row led/full-brightness :amber))
   ([launchkeymini row-data row brightness color]
     (let [sink (-> launchkeymini :rcv)]
       (render-row* sink row-data row brightness color))))
 
-(defn render-side*
-  ([sink side-data row] (render-side* sink side-data row led/full-brightness :amber))
-  ([sink side-data row brightness color]
+(defn- render-side*
+  ([sink side-data] (render-side* sink side-data led/full-brightness :amber))
+  ([sink side-data brightness color]
     (doseq [row (range 0 grid/grid-height)]
       (let [value (nth side-data row)]
         (if (= value 0)
@@ -145,15 +160,18 @@
           (led-on* sink (row->side row) brightness color))))))
 
 (defn render-side
-  ([launchkeymini side-data row] (render-side launchkeymini side-data row led/full-brightness :amber))
-  ([launchkeymini side-data row brightness color]
+  "Renders the side buttons. You have to provide the side-data
+  as seqence of length launchkey-mini.grid/grid-height"
+  ([launchkeymini side-data] (render-side launchkeymini side-data led/full-brightness :amber))
+  ([launchkeymini side-data brightness color]
     (let [sink (-> launchkeymini :rcv)]
-      (render-side* sink side-data row brightness color))))
+      (render-side* sink side-data brightness color))))
 
 (defn- id->color [id]
   (nth [:orange :red :green] (mod id 3)))
 
-(defn intromation [sink]
+(defn- intromation [sink]
+  "Runs a nice intromation on the device"
   (reset-launchkey* sink)
   (doall
     (pmap
@@ -180,3 +198,4 @@
       (led-off* sink [row col])
       (Thread/sleep 50)))
   (reset-launchkey* sink))
+
--- test/launchkey_mini/t_device.clj
+++ test/launchkey_mini/t_device.clj
@@ -3,6 +3,13 @@
             [overtone.studio.midi :refer :all :as midi]
             [launchkey-mini.device :as device]))
 
+(background
+  (#'midi/midi-note-on anything anything anything anything) => false
+  (#'midi/midi-note-on anything anything anything) => false
+  (#'midi/midi-control anything anything anything anything) => false
+  (#'midi/midi-control anything anything anything) => false)
+
+
 (fact "correct side controls"
   device/side-controls => (just [:row1 :row2] :in-any-order))
 
@@ -19,18 +26,80 @@
 (fact "side->row returns nil on wrong row"
   (device/side->row :knob1) => nil)
 
-(fact "enable-incontrol-msg has correct content"
-  (#'device/enable-incontrol-msg :note) => 0x0C
-  (#'device/enable-incontrol-msg :velocity) => 0x7F
-  (#'device/enable-incontrol-msg :type) => :note-on)
+(fact "enable-incontrol sends note on to note 0x0C with velocity 0x7F"
+  (#'device/enable-incontrol {:rcv ..sink..}) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x0C 0x7F) => true))
 
-(fact "disable-incontrol-msg has correct content"
-  (#'device/disable-incontrol-msg :note) => 0x0C
-  (#'device/disable-incontrol-msg :velocity) => 0x0
-  (#'device/disable-incontrol-msg :type) => :note-on)
+(fact "reset-launchkey sends control change to note 0 with velocity 0"
+  (#'device/reset-launchkey {:rcv ..sink..}) => truthy
+  (provided
+    (#'midi/midi-control ..sink.. 0 0) => true))
 
-(fact "returns correct led-details"
-  ((#'device/led-details [0 0]) :note) => 0x60
-  ((#'device/led-details :row1) :note) => 104
-  (#'device/led-details :foo) => nil)
+(fact "disable-incontrol sends note on to note 0x0C with velocity 0x00"
+  (#'device/disable-incontrol {:rcv ..sink..}) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x0C 0x00) => true))
 
+(fact "led-on sends note-on amber at full brightness by default for a grid entry"
+  (#'device/led-on {:rcv ..sink..} [0 0]) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x60 63) => true)
+
+  (#'device/led-on {:rcv ..sink..} [0 7]) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x67 63) => true)
+
+  (#'device/led-on {:rcv ..sink..} [1 0]) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x70 63) => true)
+
+  (#'device/led-on {:rcv ..sink..} [1 7]) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x77 63) => true))
+
+(fact "led-on sends note-on with correct color for a grid entry"
+  (#'device/led-on {:rcv ..sink..} [0 0] 3 :red) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x60 15) => true))
+
+(fact "led-on sends note-on with for a side button"
+  (#'device/led-on {:rcv ..sink..} :row1) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 104 63) => true)
+
+  (#'device/led-on {:rcv ..sink..} :row2) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 120 63) => true))
+
+(fact "led-off sends note-on with velocity 12"
+  (#'device/led-off {:rcv ..sink..} [0 0]) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 0x60 12) => true)
+
+  (#'device/led-off {:rcv ..sink..} :row1) => truthy
+  (provided
+    (#'midi/midi-note-on ..sink.. 104 12) => true))
+
+(fact "led-on-all sends midi-control to note 0 with velocity 0x7D"
+  (#'device/led-on-all {:rcv ..sink..}) => truthy
+  (provided
+    (#'midi/midi-control ..sink.. 0x00 0x7D) => true))
+
+(fact "render-row calls the correct commands"
+  (#'device/render-row {:rcv ..sink..} [0 1 0 0 0 1 0 0] 0) => nil
+  (provided
+    (#'device/led-off* ..sink.. [0 0]) => true
+    (#'device/led-on*  ..sink.. [0 1] 3 :amber) => true
+    (#'device/led-off* ..sink.. [0 2]) => true
+    (#'device/led-off* ..sink.. [0 3]) => true
+    (#'device/led-off* ..sink.. [0 4]) => true
+    (#'device/led-on*  ..sink.. [0 5] 3 :amber) => true
+    (#'device/led-off* ..sink.. [0 6]) => true
+    (#'device/led-off* ..sink.. [0 7]) => true))
+
+(fact "render-side calls the correct commands"
+  (#'device/render-side {:rcv ..sink..} [0 1]) => nil
+  (provided
+    (#'device/led-off* ..sink.. :row1) => true
+    (#'device/led-on*  ..sink.. :row2 3 :amber) => true))
