815058cab7f8e14f4f8a1b92319db531f42e3cc1
Martin Gondermann <magicmonty@pagansoft.de>
2015-03-21 22:06:13 +0100

    beat plugin renders now its beat via a given trigger
    * updated demo.clj with a demo for the beat plugin

--- doc/demo.clj
+++ doc/demo.clj
@@ -2,3 +2,42 @@
 (use 'launchkey-mini.core)
 (boot-launchkey-mini!)
 
+(do
+  (def lk (first launchkeymini-kons))
+
+  (use 'launchkey-mini.mode)
+  (enable-session-mode :default)
+
+  (use 'launchkey-mini.plugins.beat)
+
+  (defonce count-trig-id (trig-id))
+
+  (defonce root-trg-bus (control-bus "global metronome pulse"))
+  (defonce root-cnt-bus (control-bus "global metronome count"))
+  (defonce beat-trg-bus (control-bus "beat pulse (fraction of root)"))
+  (defonce beat-cnt-bus (control-bus "beat count"))
+
+  (def BEAT-FRACTION "Number of global pulses per beat" 30)
+  (def current-beat-fraction (atom BEAT-FRACTION))
+  (update-current-beat lk :default 0)
+
+  (require '[overtone.synth.timing :as timing])
+  (def r-cnt (timing/counter :in-bus root-trg-bus :out-bus root-cnt-bus))
+  (def r-trg (timing/trigger :rate 100 :in-bus root-trg-bus))
+  (def b-cnt (timing/counter :in-bus beat-trg-bus :out-bus beat-cnt-bus))
+  (def b-trg (timing/divider :div BEAT-FRACTION :in-bus root-trg-bus :out-bus beat-trg-bus))
+  (defsynth get-beat [] (send-trig (in:kr beat-trg-bus) count-trig-id (+ (in:kr beat-cnt-bus) 1)))
+
+  (add-beat-event-handlers lk 0 :default count-trig-id)
+
+  ;;Adjust bpm
+  (defn bpm+ [] (ctl b-trg :div (swap! current-beat-fraction dec)))
+  (defn bpm- [] (ctl b-trg :div (swap! current-beat-fraction inc)))
+
+  ; start beat display
+  (def thebeat (get-beat))
+)
+
+(comment
+  (kill thebeat) ; stop the beat
+)
--- src/launchkey_mini/plugins/beat.clj
+++ src/launchkey_mini/plugins/beat.clj
@@ -5,6 +5,7 @@
    edit each grid."
   (:require
    [overtone.libs.event       :as e]
+   [overtone.sc.trig          :as trig]
    [launchkey-mini.led        :as led]
    [launchkey-mini.device     :as device]
    [launchkey-mini.state-maps :as state-maps]
@@ -12,38 +13,67 @@
 
 
 (defn render-state-with-beat
-  ([launchkeymini]
+  ([launchkeymini mode-id]
    (let [state (:state launchkeymini)]
      (when-let [current-beat (:beat @state)]
-       (render-state-with-beat launchkeymini current-beat))))
+       (render-state-with-beat launchkeymini mode-id current-beat))))
 
-  ([launchkeymini beat-no]
-   (device/render-state launchkeymini)
-   (let [beat-column (mod beat-no grid/grid-width)
-         current-state (:state launchkeymini)
-         grid (state-maps/active-grid current-state)]
-     (doseq [row (range 0 grid/grid-height)]
-       (let [cell (grid/cell grid beat-column row)
-             brigthness (if (= cell 0) led/low-brightness led/full-brightness)
-             color (if (state-maps/row-active? current-state row) :green :red)]
-         (device/led-on launchkeymini [beat-column row] brigthness color))))))
+  ([launchkeymini mode-id beat-no]
+   (when (state-maps/active-mode? (:state launchkeymini) mode-id)
+     (device/render-state launchkeymini)
+     (let [beat-column (mod beat-no grid/grid-width)
+           current-state (:state launchkeymini)
+           grid (state-maps/active-grid current-state)]
+       (doseq [row (range 0 grid/grid-height)]
+         (let [cell (grid/cell grid beat-column row)
+               brigthness (if (= cell 0) led/low-brightness led/full-brightness)
+               color (if (state-maps/row-active? current-state row) :green :red)]
+           (device/led-on launchkeymini [beat-column row] brigthness color)))))))
 
-(defn add-beat-event-handlers [launchkeymini idx mode-id metro]
+(defn update-current-beat [launchkeymini mode-id beat-no]
   (let [state (:state launchkeymini)]
-    (when-let [current-beat (:beat @state)]
-      (e/on-event [device/launchkeymini-event-id idx mode-id :side]
-                  (fn [_] (render-state-with-beat launchkeymini current-beat))
-                  (str "side-beat-event-for-" idx))
+    (swap! state assoc :beat beat-no)
+    (render-state-with-beat launchkeymini mode-id)))
 
-      (e/on-event [device/launchkeymini-event-id idx mode-id :grid-on]
-                  (fn [_] (render-state-with-beat launchkeymini current-beat))
-                  (str "grid-beat-event-for-" idx)))))
-
-(defn update-current-beat [launchkeymini beat-no]
+(defn add-beat-event-handlers [launchkeymini idx mode-id count-trig-id]
   (let [state (:state launchkeymini)]
-    (swap! state assoc :beat beat-no)))
+    (trig/on-trigger count-trig-id
+                     (fn [beat] (update-current-beat launchkeymini mode-id beat))
+                     (str "beat-trigger-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :side]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "side-beat-event-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :grid-on]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "grid-beat-event-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :meta :up]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "meta-up-beat-event-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :meta :down]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "meta-down-beat-event-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :meta :left]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "meta-left-beat-event-for-" idx))
+
+    (e/on-event [device/launchkeymini-event-id idx mode-id :meta :right]
+                (fn [_] (render-state-with-beat launchkeymini mode-id))
+                (str "meta-right-beat-event-for-" idx))
+    ))
 
 (defn remove-beat-event-handlers [idx]
+  (e/remove-event-handler (str "beat-trigger-for-" idx))
+  (e/remove-event-handler (str "meta-up-beat-event-for-" idx))
+  (e/remove-event-handler (str "meta-down-beat-event-for-" idx))
+  (e/remove-event-handler (str "meta-left-beat-event-for-" idx))
+  (e/remove-event-handler (str "meta-right-beat-event-for-" idx))
   (e/remove-event-handler (str "side-beat-event-for-" idx))
   (e/remove-event-handler (str "grid-beat-event-for-" idx)))
 
+(defn start [launchkeymini mode count-trig-id ]
+  )
