9562415435b1e6e77ebe0d430cabf5fae5e88f38
Martin Gondermann <magicmonty@pagansoft.de>
2015-03-22 10:43:09 +0100

    Moved some of the more daunting parts into the beat plugin

--- doc/demo.clj
+++ doc/demo.clj
@@ -5,34 +5,6 @@
 (do
   (def lk (first launchkeymini-kons))
 
-  (use 'launchkey-mini.mode)
-  (add-mode!           lk :sequencer)
-  (enable-session-mode lk :sequencer)
-  (set-page-max-x      lk :sequencer 1)
-  (set-page-max-y      lk :sequencer 3)
-  (set-mode            lk :sequencer)
-
-  (defonce count-trig-id (trig-id))
-
-  (defonce root-trg-bus (control-bus "global metronome pulse"))
-  (defonce root-cnt-bus (control-bus "global metronome count"))
-  (defonce beat-trg-bus (control-bus "beat pulse (fraction of root)"))
-  (defonce beat-cnt-bus (control-bus "beat count"))
-
-  (def BEAT-FRACTION "Number of global pulses per beat" 30)
-  (def current-beat-fraction (atom BEAT-FRACTION))
-
-  (require '[overtone.synth.timing :as timing])
-  (def r-cnt (timing/counter :in-bus root-trg-bus :out-bus root-cnt-bus))
-  (def r-trg (timing/trigger :rate 100 :in-bus root-trg-bus))
-  (def b-cnt (timing/counter :in-bus beat-trg-bus :out-bus beat-cnt-bus))
-  (def b-trg (timing/divider :div BEAT-FRACTION :in-bus root-trg-bus :out-bus beat-trg-bus))
-  (defsynth get-beat [] (send-trig (in:kr beat-trg-bus) count-trig-id (+ (in:kr beat-cnt-bus) 1)))
-
-  ;;Adjust bpm
-  (defn bpm+ [] (ctl b-trg :div (swap! current-beat-fraction dec)))
-  (defn bpm- [] (ctl b-trg :div (swap! current-beat-fraction inc)))
-
   (def tom-electro-s       (sample (freesound-path 108001)))
   (def sizzling-high-hat-s (sample (freesound-path 44859)))
   (def kick-s              (sample (freesound-path 777)))
@@ -43,12 +15,8 @@
 
   (def samples-set [tom-electro-s sizzling-high-hat-s hip-hop-kick-s clap-s bell-s snare-s])
 
-  (use 'launchkey-mini.plugins.sequencer)
-  (def lk-sequencer (mk-sequencer "launchkey-sequencer" samples-set 16 beat-cnt-bus beat-trg-bus 0))
-
   (use 'launchkey-mini.plugins.beat)
-  (update-current-beat lk :sequencer 0)
-  (add-beat-event-handlers lk 0 :sequencer count-trig-id)
+  (mk-sequencer-mode lk 0 :sequencer 16 samples-set)
 
   ; start beat display
   (def thebeat (get-beat))
--- src/launchkey_mini/plugins/beat.clj
+++ src/launchkey_mini/plugins/beat.clj
@@ -3,22 +3,26 @@
    When playing scroll through > 8 phrase beats. Either
    edit beats live as they play or switch to session mode and
    edit each grid."
-  (:use [launchkey-mini.plugins.sequencer])
+  (:use [launchkey-mini.plugins.sequencer]
+        [overtone.core :only [control-bus defsynth send-trig in:kr ctl]])
   (:require
    [overtone.libs.event       :as e]
    [overtone.sc.trig          :as trig]
    [launchkey-mini.led        :as led]
    [launchkey-mini.device     :as device]
+   [launchkey-mini.mode       :as mode]
    [launchkey-mini.state-maps :as state-maps]
-   [launchkey-mini.grid       :as grid]))
+   [launchkey-mini.grid       :as grid]
+   [overtone.synth.timing     :as timing]))
 
 (def phrase-size 16)
 
 (defn toggle-row [launchkeymini sequencer absolute-row]
   (let [state (:state launchkeymini)]
-    (if (state-maps/absolute-row-active? state absolute-row)
-      (sequencer-write! sequencer absolute-row (take phrase-size (state-maps/complete-row state absolute-row)))
-      (reset-pattern! sequencer absolute-row))))
+    (when (< absolute-row (:num-samples sequencer))
+      (if (state-maps/absolute-row-active? state absolute-row)
+        (sequencer-write! sequencer absolute-row (take phrase-size (state-maps/complete-row state absolute-row)))
+        (reset-pattern! sequencer absolute-row)))))
 
 (defn toggle-rows [launchkeymini sequencer]
   (doseq [absolute-row (range 0 (state-maps/y-max (:state launchkeymini)))]
@@ -96,3 +100,55 @@
 
 (defn off [sequencer]
   (reset-all-patterns! sequencer))
+
+(defn- add-sequencer-mode [launchkeymini mode-id phrase-length num-samples]
+  (when-not (mode/mode? launchkeymini mode-id)
+    (def phrase-size phrase-length)
+    (mode/add-mode!           launchkeymini mode-id)
+    (mode/enable-session-mode launchkeymini mode-id)
+    (mode/set-page-max-x      launchkeymini mode-id (int (/ phrase-size grid/grid-width)))
+    (mode/set-page-max-y      launchkeymini mode-id (int (/ num-samples grid/grid-height)))
+    (mode/set-mode            launchkeymini mode-id)))
+
+(defn create-global-metronome []
+  (defonce count-trig-id (trig/trig-id))
+
+  (defonce root-trg-bus (control-bus "global metronome pulse"))
+  (defonce root-cnt-bus (control-bus "global metronome count"))
+  (defonce beat-trg-bus (control-bus "beat pulse (fraction of root)"))
+  (defonce beat-cnt-bus (control-bus "beat count"))
+
+  (def BEAT-FRACTION "Number of global pulses per beat" 30)
+  (def current-beat-fraction (atom BEAT-FRACTION))
+
+  (require '[overtone.synth.timing :as timing])
+  (def r-cnt (timing/counter :in-bus root-trg-bus :out-bus root-cnt-bus))
+  (def r-trg (timing/trigger :rate 100 :in-bus root-trg-bus))
+  (def b-cnt (timing/counter :in-bus beat-trg-bus :out-bus beat-cnt-bus))
+  (def b-trg (timing/divider :div BEAT-FRACTION :in-bus root-trg-bus :out-bus beat-trg-bus))
+  (defsynth get-beat [] (send-trig (in:kr beat-trg-bus) count-trig-id (+ (in:kr beat-cnt-bus) 1)))
+
+  ;;Adjust bpm
+  (defn bpm+ [] (ctl b-trg :div (swap! current-beat-fraction dec)))
+  (defn bpm- [] (ctl b-trg :div (swap! current-beat-fraction inc))))
+
+(defn mk-sequencer-mode [launchkeymini idx mode-id phrase-length samples-set]
+  (create-global-metronome)
+  (add-sequencer-mode launchkeymini
+                      mode-id
+                      phrase-length
+                      (count samples-set))
+  (update-current-beat launchkeymini mode-id 0)
+
+  (def beat-sequencer (mk-sequencer (str mode-id "-beat-sequencer")
+                                    samples-set
+                                    phrase-length
+                                    beat-cnt-bus
+                                    beat-trg-bus
+                                    0))
+
+  (add-beat-event-handlers launchkeymini
+                           idx
+                           mode-id
+                           count-trig-id
+                           beat-sequencer))
